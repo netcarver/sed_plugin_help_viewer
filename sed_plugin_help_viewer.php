921958a72aa2a22c50b6befc0293f2048cd4a8c8
netcarver <nc.github@net-carving.com>
2008-07-09 21:06:38 +0800

    Switch to Code-then-help order

--- sed_plugin_help_viewer.php
+++ sed_plugin_help_viewer.php
@@ -10,57 +10,6 @@ $plugin['type'] = 1;
 
 @include_once('../zem_tpl.php');
 
-if (0) {
-?>
-<!-- CSS & HELP
-# --- BEGIN PLUGIN CSS ---
-<style type="text/css">
-div#sed_help td { vertical-align:top; }
-div#sed_help code { font-weight:bold; font: 105%/130% "Courier New", courier, monospace; background-color: #FFFFCC;}
-div#sed_help code.sed_code_tag { font-weight:normal; border:1px dotted #999; background-color: #f0e68c; display:block; margin:10px 10px 20px; padding:10px; }
-div#sed_help a:link, div#sed_help a:visited { color: blue; text-decoration: none; border-bottom: 1px solid blue; padding-bottom:1px;}
-div#sed_help a:hover, div#sed_help a:active { color: blue; text-decoration: none; border-bottom: 2px solid blue; padding-bottom:1px;}
-div#sed_help h1 { color: #369; font: 20px Georgia, sans-serif; margin: 0; text-align: center; }
-div#sed_help h2 { border-bottom: 1px solid black; padding:10px 0 0; color: #369; font: 17px Georgia, sans-serif; }
-div#sed_help h3 { color: #693; font: bold 12px Arial, sans-serif; letter-spacing: 1px; margin: 10px 0 0;text-transform: uppercase;}
-</style>
-# --- END PLUGIN CSS ---
-# --- BEGIN PLUGIN HELP ---
-<div id="sed_help">
-
-h1(#intro). Plugin Help Section Viewer.
-
-sed_plugin_help_viewer plugin, v0.3 (April 2nd, 2007)
-
-Allows you to view the help section of any plugin in your cache directory.
-
-If the file matches ZEM's template then the help section will get run through the textile formatter before display, otherwise it will be treated as straight HTML.
-
-h2(#versions). Version History
-
-v0.3
-
-* Updated to allow processing of TxP 4.0.4 plugins marked as @allow_html_help@.
-
-v0.2
-
-* Fixed undefined variable error when cache directory is empty. (Thanks Rigel.)
-
-v0.1 Implements the following features&#8230;
-
-* Scanning the cache directory for plugins.
-** When clicked, pull out the help section, Textile it if needed, display it.
-
-h2(#credits). Credits
-
-Sections of this plugin use code from <strong>Alex (a.k.a. Zem)</strong> and <strong>Yura (a.k.a Inspired)</strong> with permission.<br/>Many thanks for helping the community guys!
-
-</div>
-# --- END PLUGIN HELP ---
--->
-<?php
-}
-
 # --- BEGIN PLUGIN CODE ---
 if(@txpinterface == 'admin') {
 	add_privs('sed_plugin_help_viewer','1,2');
@@ -160,12 +109,13 @@ function _zem_extract_section($lines, $section) {
 	return join("\n", $content);
 	}
 
-function _ied_extract_section($lines, $section) {
-//	$meta_delim = '--- PLUGIN METADATA ---';
+function _ied_extract_section($lines, $section) 
+	{
+	#$meta_delim = '--- PLUGIN METADATA ---';
 	$help_delim = '--- BEGIN PLUGIN HELP ---';
 	$end_delim  = '--- END PLUGIN HELP & METADATA ---';
 
-//	$code_start = 1;
+	#$code_start = 1;
 	$help_line = array_search($help_delim, $lines);
 	if( false === $help_line )
 		return '';	// This is not an ied file.
@@ -229,5 +179,50 @@ function _sed_list_plugins_from_cache($message='') {
 	}
 
 # --- END PLUGIN CODE ---
+/*
+# --- BEGIN PLUGIN CSS ---
+<style type="text/css">
+div#sed_help td { vertical-align:top; }
+div#sed_help code { font-weight:bold; font: 105%/130% "Courier New", courier, monospace; background-color: #FFFFCC;}
+div#sed_help code.sed_code_tag { font-weight:normal; border:1px dotted #999; background-color: #f0e68c; display:block; margin:10px 10px 20px; padding:10px; }
+div#sed_help a:link, div#sed_help a:visited { color: blue; text-decoration: none; border-bottom: 1px solid blue; padding-bottom:1px;}
+div#sed_help a:hover, div#sed_help a:active { color: blue; text-decoration: none; border-bottom: 2px solid blue; padding-bottom:1px;}
+div#sed_help h1 { color: #369; font: 20px Georgia, sans-serif; margin: 0; text-align: center; }
+div#sed_help h2 { border-bottom: 1px solid black; padding:10px 0 0; color: #369; font: 17px Georgia, sans-serif; }
+div#sed_help h3 { color: #693; font: bold 12px Arial, sans-serif; letter-spacing: 1px; margin: 10px 0 0;text-transform: uppercase;}
+</style>
+# --- END PLUGIN CSS ---
+# --- BEGIN PLUGIN HELP ---
+<div id="sed_help">
 
+h1(#intro). Plugin Help Section Viewer.
+
+sed_plugin_help_viewer plugin, v0.3 (April 2nd, 2007)
+
+Allows you to view the help section of any plugin in your cache directory.
+
+If the file matches ZEM's template then the help section will get run through the textile formatter before display, otherwise it will be treated as straight HTML.
+
+h2(#versions). Version History
+
+v0.3
+
+* Updated to allow processing of TxP 4.0.4 plugins marked as @allow_html_help@.
+
+v0.2
+
+* Fixed undefined variable error when cache directory is empty. (Thanks Rigel.)
+
+v0.1 Implements the following features&#8230;
+
+* Scanning the cache directory for plugins.
+** When clicked, pull out the help section, Textile it if needed, display it.
+
+h2(#credits). Credits
+
+Sections of this plugin use code from <strong>Alex (a.k.a. Zem)</strong> and <strong>Yura (a.k.a Inspired)</strong> with permission.<br/>Many thanks for helping the community guys!
+
+</div>
+# --- END PLUGIN HELP ---
+*/
 ?>
